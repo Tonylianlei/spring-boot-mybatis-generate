24ffef4c9a24619069022a9ae054ac48e658972f
Tony <tony.lian@chinaventure.com.cn>
2018-11-06 15:44:05 +0800

    初步提交

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 *开 发 者：连磊
+	 *开发时间：2018/11/6 15:43
+	 *描    述：开始
+	 **/
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
